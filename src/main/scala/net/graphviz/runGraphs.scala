318aaa0ac56c8cf201714c84c4d8e98d78f18907
Master Control <fireflightning@gmail.com>
2017-08-10 08:58:46 +0200

    added code for generating simple graph

--- src/main/scala/net/graphviz/runGraphs.scala
+++ src/main/scala/net/graphviz/runGraphs.scala
@@ -1,9 +1,27 @@
 package net.graphviz
 
+import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
+import sys.process._
+
 object myMainClass{
     def main(args: Array[String]) {
+        
+        // create dot object with default graph params
+        val dot = new Dot()
 
-        println("yo whuddup")
+        // add nodes
+        dot.nodeBuffer += Node.apply("Module 1")
+        dot.nodeBuffer += Node.apply("Module 2")
+
+        // add edges
+        dot.edgeBuffer += Edge.apply("Module 1", "Module 2")
+
+        // create .dot file and compile
+        Files.write(Paths.get("src/main/resources/graph.dot"), 
+            dot.toString.getBytes(StandardCharsets.UTF_8))
+
+        "neato src/main/resources/graph.dot -Tpng -osrc/main/resources/graph.png " !
 
     }
 }
