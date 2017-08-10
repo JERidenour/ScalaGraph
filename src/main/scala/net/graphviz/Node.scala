f5d99127e8ab5c8635675f3af3781d69948d4826
Master Control <fireflightning@gmail.com>
2017-08-10 08:39:39 +0200

    removed some character replacements in Node

--- src/main/scala/net/graphviz/Node.scala
+++ src/main/scala/net/graphviz/Node.scala
@@ -42,13 +42,6 @@ object Node {
             .replace("""'""", "")
             .replace("""[""", "")
             .replace("""]""", "")
-            .replace("$", "S")
-            .replace("2", "two")
-            .replace("50", "fifty")
-            .replace("7", "seven")
-            .replace("69", "sixtynine")
-            .replace("1", "one")
-
     }
 
     def createLabel(input: String) = {
